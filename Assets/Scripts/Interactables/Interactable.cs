d0d8da994c266cd8b54158397e7a070b847104d0
LWCoding <DiamondMob01@gmail.com>
2024-05-14 14:40:54 -0700

    Updated wire interactable to hide popup, made laser interactable update every frame

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -9,6 +9,7 @@ public abstract class Interactable : MonoBehaviour
     [Header("(Optional) Popup Assignment")]
     public GameObject PopupObject;
 
+    [HideInInspector] public bool ShouldShowPopup = true;
     [HideInInspector] public bool IsInteractable = false;  //  Can be checked by other scripts
                                                            //  to see if this is interactable
 
@@ -25,6 +26,7 @@ public abstract class Interactable : MonoBehaviour
     /// </summary>
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!ShouldShowPopup) { return;  }
         IsInteractable = true;
         if (PopupObject != null && collision.gameObject.CompareTag("Player"))
         {
@@ -35,7 +37,7 @@ public abstract class Interactable : MonoBehaviour
     /// <summary>
     /// If we stop colliding with the player, hide the popup.
     /// </summary>
-    private void OnTriggerExit2D(Collider2D collision)
+    public void OnTriggerExit2D(Collider2D collision)
     {
         IsInteractable = false;
         if (PopupObject != null && collision.gameObject.CompareTag("Player"))
--- Assets/Scripts/Interactables/WireInteractable.cs
+++ Assets/Scripts/Interactables/WireInteractable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WireInteractable : Interactable
@@ -20,6 +21,14 @@ public class WireInteractable : Interactable
     {
         if (IsInteractable && Input.GetKeyDown(KeyCode.E))
         {
+            if (OnlyWorksOnce)
+            {
+                ShouldShowPopup = false;  //  Hide popup if this has been interacted with
+                                          //  and it should only be interacted with ONCE
+                PopupObject.SetActive(false);
+                // Turns the wires gray; this can be deleted later
+                GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.8f, 0.8f);  
+            }
             if (TriggerableToTrigger != null && (!OnlyWorksOnce || !_wasActivated))
             {
                 _wasActivated = true;
