037a087e7fd8726096c9b45ff09b130b54b794a6
LWCoding <DiamondMob01@gmail.com>
2024-05-14 15:13:29 -0700

    Moved changes to main

--- Assets/Scripts/Footsteps.cs
+++ Assets/Scripts/Footsteps.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Footsteps : MonoBehaviour
 {
-    public AudioSource audioSource; // Reference to the AudioSource footsteps component
-    public Rigidbody2D rb2D; // Reference to the critter
 
     public AudioClip[] footstepNoises;
+    private AudioSource _audioSource;
     public float footstepVolume;
     private float _delaySinceLastFootstep;
 
     private const float TIME_BETWEEN_FOOTSTEP_NOISES = 0.16f;
 
-    void Start()
+    void Awake()
     {
-        // Initialize the AudioSource and Rigidbody2D components if not set
-        if (audioSource == null)
-            audioSource = GetComponent<AudioSource>();
-        if (rb2D == null)
-            rb2D = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -27,7 +23,7 @@ public class Footsteps : MonoBehaviour
         // Check if the player is moving
         if (Input.GetAxis("Horizontal") != 0 && Time.time - _delaySinceLastFootstep > TIME_BETWEEN_FOOTSTEP_NOISES) // Adjust the threshold as needed
         {
-            audioSource.PlayOneShot(footstepNoises[Random.Range(0, footstepNoises.Length)], footstepVolume);
+            _audioSource.PlayOneShot(footstepNoises[Random.Range(0, footstepNoises.Length)], footstepVolume);
             _delaySinceLastFootstep = Time.time;
         }
     }
