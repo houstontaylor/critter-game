6edfab4446daeaa8b8d8cc860114d71fb9b1671b
LWCoding <DiamondMob01@gmail.com>
2024-05-28 15:37:34 -0700

    Rails now draw between control points

--- Assets/Scripts/MainRailRenderer.cs
+++ Assets/Scripts/MainRailRenderer.cs
@@ -7,7 +7,7 @@ public class MainRailRenderer : MonoBehaviour
 {
 
     private LineRenderer _lineRenderer;  // This sprite's line renderer to show rails
-    private GameObject[] _controlPoints;
+    private List<GameObject> _controlPoints;
 
     private void Awake()
     {
@@ -16,14 +16,15 @@ public class MainRailRenderer : MonoBehaviour
 
     private void Start()
     {
-        _controlPoints = GameObject.FindGameObjectsWithTag("RailCP");
+        _controlPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("RailCP"));
+        _controlPoints.Sort((x, y) => (int)(y.transform.position.x - x.transform.position.x));
         RenderRails();  // Render rails ONCE at the beginning of the game
     }
 
    private void RenderRails()
     {
-        _lineRenderer.positionCount = _controlPoints.Length;
-        for (int i = 0; i < _controlPoints.Length; i++)
+        _lineRenderer.positionCount = _controlPoints.Count;
+        for (int i = 0; i < _controlPoints.Count; i++)
         {
             _lineRenderer.SetPosition(i, _controlPoints[i].transform.position);
         }
