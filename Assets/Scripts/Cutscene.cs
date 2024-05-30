bd0a5eebd9c8803fd22674b2f87a1b65362a91c7
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-05-30 15:31:14 -0700

    Started on cutscene

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -5,5 +5,15 @@ public class Cutscene : MonoBehaviour
     public void Play() {
         // TODO: Iterate through all the children of the cutscene object, fading them in one after another
         Debug.Log("Playing cutscene");
+
+        enabled = true;
+        foreach (Transform child in transform) {
+            // Fade in the child
+            var spriteRenderer = child.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(1, 1, 1, 0);
+            // TODO: Animate
+            spriteRenderer.color = new Color(1, 1, 1, 1);
+        }
+        enabled = false;
     }
 }
