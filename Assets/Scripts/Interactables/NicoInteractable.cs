a779d7b9a939aef47a99b1b56492a96949c2a871
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-06-02 23:14:21 -0700

    Add hard drive gate to level 4 and fix hard drive bugs

--- Assets/Scripts/Interactables/NicoInteractable.cs
+++ Assets/Scripts/Interactables/NicoInteractable.cs
@@ -6,35 +6,20 @@ public class Nico : ItemTaker
 
     public override void Interact()
     {
-        // Get the cutscene from the hard drive that the player is holding
         Cutscene cutscene = GetItem().GetComponent<HardDrive>().cutscene;
-        if (cutscene == null) {
-            Debug.LogError("Hard drive does not have a cutscene set");
-            return;
-        }
-
         RailControlPoint stop = GetItem().GetComponent<HardDrive>().stop;
-        if (stop == null) {
-            Debug.LogError("Hard drive does not have a stop set");
-            return;
-        }
-
         DoorTriggerable door = GetItem().GetComponent<HardDrive>().door;
-        if (door == null) {
-            Debug.LogError("Hard drive does not have a door set");
-            return;
-        }
 
         if (!TakeItem()) {
             return;
         }
 
-        // TODO: Pause
-        cutscene.Play();
-        // TODO: unpause
-
-        door.Interact();
-        stop.Continue();
+        if (cutscene != null)
+            cutscene.Play();
+        if (door != null)
+            door.Interact();
+        if (stop != null)
+            stop.Continue();
     }
 
 }
