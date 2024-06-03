c6a990c8d3e62c7d86f295850adb3176ca1b958b
LWCoding <DiamondMob01@gmail.com>
2024-06-02 22:53:57 -0700

    Nico eyes work now and made parts of Level 2 look less dumb :3

--- Assets/Scripts/Interactables/BrokenNicoInteractable.cs
+++ Assets/Scripts/Interactables/BrokenNicoInteractable.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(NicoEyes))]
 public class BrokenNicoInteractable : ItemTaker
 {
     public AudioClip powerUpSound;
     public float powerUpVolume;
     private AudioSource audioSource;
 
-    public override string ItemName { get { return "Battery"; } }
+    private NicoEyes _nicoEyes;
+
+    public override string ItemName { get => "Battery"; }
+
     private new void Start() {
         base.Start();
+        _nicoEyes = GetComponent<NicoEyes>();
         audioSource = GetComponent<AudioSource>();
+        _nicoEyes.enabled = false;
     }
     public override void Interact()
     {
@@ -26,6 +32,7 @@ public class BrokenNicoInteractable : ItemTaker
         // Shift Nico into its next state
         // FIXME: Need to walk out and back in to pick up after giving battery
         Destroy(this); // removes this script
+        _nicoEyes.enabled = true;  // Make the eyes render
         Pickupable pickupable = gameObject.AddComponent<Pickupable>();
         pickupable.PopupObject = PopupObject; // Needed to set up the script properly
         pickupable.ShowPopup();  // You'll be touching Nico when you fix him, so...
--- Assets/Scripts/NicoEyes.cs
+++ Assets/Scripts/NicoEyes.cs
@@ -9,19 +9,39 @@ public class NicoEyes : MonoBehaviour
     [SerializeField] private Transform _eyeObject;
 
     [HideInInspector] public PlayerController _playerController;
+
     public void Awake()
     {
         _playerController = FindObjectOfType<PlayerController>();
     }
 
+    /// <summary>
+    /// Hide eyes when this component is disabled.
+    /// </summary>
+    private void OnDisable()
+    {
+        _eyeParent.gameObject.SetActive(false);
+        _eyeObject.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Show eyes when this component is enabled.
+    /// </summary>
+    private void OnEnable()
+    {
+        _eyeParent.gameObject.SetActive(true);
+        _eyeObject.gameObject.SetActive(true);
+    }
+
     private void Update()
     {
         Vector3 targetPos = _playerController.transform.position;
-        _eyeParent.right = targetPos - transform.position;
+        _eyeParent.right = targetPos - transform.position;  // Rotate to point towards critter
+
         Vector3 rot = _eyeParent.rotation.eulerAngles;
-        float value = Mathf.Clamp(rot.z + 90, 360 - 50, 360 + 50);
+        float value = Mathf.Clamp(rot.z + 90, 360 - 20, 360 + 20);  // Amount to rotate
+
         _eyeParent.transform.rotation = Quaternion.Euler(rot.x, rot.y, value);
-        _eyeObject.transform.rotation = Quaternion.Euler(rot.x, rot.y, -value * 2);
     }
 
 }
