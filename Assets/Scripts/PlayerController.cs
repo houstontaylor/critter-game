397a7d7ba39487ba8a1f94c0ec6814dc11448d2a
LWCoding <DiamondMob01@gmail.com>
2024-06-08 22:30:41 -0700

    Removing this thing because it's preventing my build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
-
-
 public class PlayerController : MonoBehaviour
 {
 
